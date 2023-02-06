cfb09b6a7d68014c5a9ff0de7fc735e6a9d28372
D3Ext <67125397+D3Ext@users.noreply.github.com>
2023-02-06 18:19:13 +0100

    Update exec.go

--- exec/exec.go
+++ exec/exec.go
@@ -1,14 +1,1 @@
 package exec
-
-import (
-  ex "os/exec"
-)
-
-func ExecuteCommand(comm string) (string) { // Return stdout and stderr
-  var out []byte
-  cmd := ex.Command("/bin/bash", "-c", comm)
-  out, _ = cmd.CombinedOutput()
-  return string(out)
-}
-
-
